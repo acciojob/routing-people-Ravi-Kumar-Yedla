4820df4bfa9ba44a9dff7418e541c1a791893f8e
Ravi-Kumar-Yedla <ravikumaryedla292@gmail.com>
2024-09-14 12:23:27 +0530

    final commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,30 @@
 
 import React from "react";
 import './../styles/App.css';
+import { BrowserRouter as Router,Route,Routes } from "react-router-dom";
+import Userdetail from "./Userdetail";
+import UserList from "./Userlist";
+
+
+const users = [
+  {id:1, Name:'ravi',Username:'sam',Email:'ravi@123.com',Phone:'756-944-34-31',Website:'ravi.org'},
+  {id:2, Name:'shiva',Username:'sam',Email:'ravi@123.com',Phone:'756-944-34-31',Website:'ravi.org'},
+  {id:3, Name:'suraj',Username:'sam',Email:'ravi@123.com',Phone:'756-944-34-31',Website:'ravi.org'},
+  {id:4, Name:'naveen',Username:'sam',Email:'ravi@123.com',Phone:'756-944-34-31',Website:'ravi.org'},
+  {id:5, Name:'madhu',Username:'sam',Email:'ravi@123.com',Phone:'756-944-34-31',Website:'ravi.org'}
+]
+
 
 const App = () => {
   return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
+    <Router>
+    
+        <Routes>
+          <Route path = "/" element = {<UserList users={users} />}/>
+          <Route path = "/users/:id" element = {<Userdetail users={users} />}/>
+        </Routes>
+    
+    </Router>
   )
 }
 
---
+++ src/components/Userdetail.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { useParams } from 'react-router-dom';
+
+const Userdetail = ({ users }) => {
+  const { id } = useParams();
+  const user = users.find(u => u.id === parseInt(id));
+
+  if (!user) {
+    return <h2>User not found</h2>;
+  }
+  return (
+    <div>
+      <h1>User Detail</h1>
+      <p><strong>Name:</strong>{user.Name}</p>
+      <p><strong>Username:</strong>{user.Username}</p>
+      <p><strong>Email:</strong>{user.Email}</p>
+      <p><strong>Phone:</strong>{user.Phone}</p>
+      <p><strong>Website:</strong>{user.Website}</p>
+    </div>
+  )
+}
+
+export default Userdetail
