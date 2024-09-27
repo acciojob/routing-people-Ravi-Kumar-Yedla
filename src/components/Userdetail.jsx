d3736731ad1f6a9dfb81c9cb719354618ad23136
Ravi-Kumar-Yedla <ravikumaryedla292@gmail.com>
2024-09-27 22:37:07 +0530

    bas bhai

--- src/components/Userdetail.jsx
+++ src/components/Userdetail.jsx
@@ -1,22 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 
 const Userdetail = ({ users }) => {
   const { id } = useParams();
-  const user = users.find(u => u.id === parseInt(id));
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const foundUser = users.find(u => u.id === parseInt(id));
+    setUser(foundUser);
+  }, [id, users]);
 
   if (!user) {
-    return <h2>User not found</h2>;
+    return <div>Loading...</div>;
   }
+
   return (
     <div>
-      
       <h1>User Detail</h1>
-      <p><strong>Name:</strong>{user.Name}</p>
-      <p><strong>Username:</strong>{user.Username}</p>
-      <p><strong>Email:</strong>{user.Email}</p>
-      <p><strong>Phone:</strong>{user.Phone}</p>
-      <p><strong>Website:</strong>{user.Website}</p>
+      <p><strong>Name: </strong>{user.Name}</p>
+      <p><strong>Username: </strong>{user.Username}</p>
+      <p><strong>Email: </strong>{user.Email}</p>
+      <p><strong>Phone: </strong>{user.Phone}</p>
+      <p><strong>Website: </strong>{user.Website}</p>
     </div>
   )
 }
